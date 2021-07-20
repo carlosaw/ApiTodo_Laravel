de94057c239c8488eaf696c9134508f88f1bc8fa
unknown <carlosfreevg@gmail.com>
2021-07-19 21:15:19 -0400

    Autenticacao

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,18 @@
 <?php
+/* Comandos para instalar SANCTUM */
+/* composer require laravel/sanctum */
+/* php artisan vendor:publish --provider="Laravel\Sanctum\SanctumServiceProvider" */
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ApiController;
+use App\Http\Controllers\AuthController;
 
 Route::get('/ping', function(){
   return ['pong' => true];
 });
+
+Route::post('/user', [AuthController::class, 'create']);
 
 Route::post('/todo', [ApiController::class, 'createTodo']);
 Route::get('/todos', [ApiController::class, 'readAllTodos']);
@@ -25,3 +31,19 @@ Route::delete('/todo/{id}', [ApiController::class, 'deleteTodo']);
 // GET /todo/{id} = Ler uma tarefa específica no sistema
 // PUT /todo/{id} = Atualizar uma tarefa no sistema
 // DELETE /todo/{id} = Deletar uma tarefa no sistema
+
+/*
+Autenticação via API
+- Em uma API bem criada, não existe sessão.
+
+- Funciona através de TOKEN:
+-- Usuário faz o login e acertaq as credenciais.
+-- O sistema gera um token e associa ao usuário.
+-- O token é retornado pela API.
+-- A partir daí:
+--- Toda requisição, será acompanhada pelo token.
+
+- O token é enviado de duas formas:
+-- No corpo da requisição (ou nos parâmetros).
+-- Nos cabeçalhos da requisição. Bearer
+*/
